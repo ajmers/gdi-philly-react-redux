7a7d607e4f2e3f6254e92acc92c554acf5028817
Anne Maiale <amaiale@rjmetrics.com>
2017-01-26 09:34:28 -0500

    changes to app js

--- sampleApp/src/App.js
+++ sampleApp/src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  onIntroClick = () => {
+    alert('CLICKED');
+  };
   render() {
     return (
       <div className="App">
@@ -10,7 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p onClick={this.onIntroClick} className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
